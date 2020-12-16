ab36433f5ffbb5e9672ac43c688ccfb92331a8ce
James Sapp <jameseisapp@gmail.com>
2020-12-15 22:24:28 -0600

    commit tree fa54c7cd35134eb4b5166c3fee23956eee300bf3

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry' 
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,70 @@ class Song
     self.class.all << self
   end
 
-end
+  def self.create
+    song = Song.new 
+    song.save
+    song  
+  end 
+
+  def self.new_by_name(new_by_name)
+    song = self.new
+    song.name = new_by_name
+    song 
+  end
+
+  def self.create_by_name(create_by_name)
+    song = self.create 
+    song.name = create_by_name
+    song 
+  end 
+
+  def self.find_by_name(song)
+    @@all.find do |song_name|
+      song_name.name == song
+    end 
+  end 
+
+  def self.find_or_create_by_name(song)
+    self.find_by_name(song) || self.create_by_name(song)
+  end 
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end  
+  end 
+
+  # new file format --> "Name - Song.mp3"
+  # not sure how to use gsub yet
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+    
+    song = self.new 
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end 
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear 
+  end 
+
+end 
+
+# song = Song.create
+
+#binding.pry 
